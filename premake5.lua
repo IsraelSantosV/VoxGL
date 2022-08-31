d87922104ec0ddbfdae5685d53e5fdaf06fd1593
Israel Santos Vieira <IsraelS_Vieira@hotmail.com>
2022-08-31 11:12:56 -0300

    Premake adicionado

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "VoxGL"
+	architecture "x64"
+	startproject "Sandbox"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "VoxGL"
+	location "VoxGL"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/Source/**.h",
+		"%{prj.name}/Source/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/ThirdParty/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"VOX_PLATFORM_WINDOWS",
+			"VOX_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
+		}
+
+	filter "configurations:Debug"
+		defines "VOX_DEBUG"
+		symbols "On"
+	
+	filter "configurations:Release"
+		defines "VOX_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "VOX_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/Source/**.h",
+		"%{prj.name}/Source/**.cpp",
+	}
+
+	includedirs
+	{
+		"VoxGL/ThirdParty/spdlog/include",
+		"VoxGL/Source"
+	}
+
+	links
+	{
+		"VoxGL"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"VOX_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "VOX_DEBUG"
+		symbols "On"
+	
+	filter "configurations:Release"
+		defines "VOX_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "VOX_DIST"
+		optimize "On"
