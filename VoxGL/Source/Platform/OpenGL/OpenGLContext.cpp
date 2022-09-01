edd90062b4507fe27f595a48f61584642d26223d
Israel Santos Vieira <IsraelS_Vieira@hotmail.com>
2022-09-01 18:57:04 -0300

    OpenGL Context adicionado

---
+++ VoxGL/Source/Platform/OpenGL/OpenGLContext.cpp
@@ -0,0 +1,31 @@
+#include "VoxPch.h"
+#include "OpenGLContext.h"
+
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+#include <gl/GL.h>
+
+namespace Vox
+{
+	OpenGLContext::OpenGLContext(GLFWwindow* windowHandle) : m_WindowHandle(windowHandle)
+	{
+		VOX_CORE_ASSERT(windowHandle, "Window handle is null!");
+	}
+
+	void OpenGLContext::Init()
+	{
+		glfwMakeContextCurrent(m_WindowHandle);
+		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+		VOX_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		LOG_CORE_INFO("OpenGL Info:");
+		LOG_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
+		LOG_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
+		LOG_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
+	}
+
+	void OpenGLContext::SwapBuffers()
+	{
+		glfwSwapBuffers(m_WindowHandle);
+	}
+}
