7759c42d29588893d5a08d1e96f90d7062fca351
Israel Santos Vieira <IsraelS_Vieira@hotmail.com>
2022-09-01 10:34:47 -0300

    Input polling e KeyCodes adicionados

---
+++ VoxGL/Source/Vox/MouseButtonCodes.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define IN_MOUSE_BUTTON_1         0
+#define IN_MOUSE_BUTTON_2         1
+#define IN_MOUSE_BUTTON_3         2
+#define IN_MOUSE_BUTTON_4         3
+#define IN_MOUSE_BUTTON_5         4
+#define IN_MOUSE_BUTTON_6         5
+#define IN_MOUSE_BUTTON_7         6
+#define IN_MOUSE_BUTTON_8         7
+#define IN_MOUSE_BUTTON_LAST      IN_MOUSE_BUTTON_8
+#define IN_MOUSE_BUTTON_LEFT      IN_MOUSE_BUTTON_1
+#define IN_MOUSE_BUTTON_RIGHT     IN_MOUSE_BUTTON_2
+#define IN_MOUSE_BUTTON_MIDDLE    IN_MOUSE_BUTTON_3
