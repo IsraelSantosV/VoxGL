0f2e6ea7c325bca19e95c37f264cd1edf12ae1d8
Israel Santos Vieira <IsraelS_Vieira@hotmail.com>
2022-08-31 23:02:35 -0300

    ImGui adicionado com controle por eventos

--- VoxGL/Source/Vox.h
+++ VoxGL/Source/Vox.h
@@ -4,6 +4,8 @@
 #include "Vox/Layer.h"
 #include "Vox/Log.h"
 
+#include "Vox/ImGui/ImGuiLayer.h"
+
 // ENTRY POINT
 #include "Vox/EntryPoint.h"
 //------------
---
+++ VoxGL/Source/Vox/ImGui/ImGuiLayer.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Vox/Layer.h"
+
+#include "Vox/Events/AplicationEvent.h"
+#include "Vox/Events/KeyEvent.h"
+#include "Vox/Events/MouseEvent.h"
+
+namespace Vox
+{
+	class VOX_API ImGuiLayer : public Layer
+	{
+	public:
+		ImGuiLayer();
+		~ImGuiLayer();
+
+		void OnAttach();
+		void OnDetach();
+		void OnUpdate();
+		void OnEvent(Event& event);
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizedEvent(WindowResizeEvent& e);
+	private:
+		float m_Time = 0.0f;
+	};
+}
