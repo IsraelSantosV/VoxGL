f69c28c432af0fbec65a5c09a6e324839a4fd323
Israel Santos Vieira <IsraelS_Vieira@hotmail.com>
2022-09-02 10:17:29 -0300

    VertexArray adicionado

--- VoxGL/Source/Vox/Application.h
+++ VoxGL/Source/Vox/Application.h
@@ -11,6 +11,7 @@
 
 #include "Vox/Renderer/Shader.h"
 #include "Vox/Renderer/Buffer.h"
+#include "Vox/Renderer/VertexArray.h"
 
 namespace Vox
 {
@@ -36,10 +37,12 @@ namespace Vox
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray;
-		std::unique_ptr<Shader> m_Shader;
-		std::unique_ptr<VertexBuffer> m_VertexBuffer;
-		std::unique_ptr<IndexBuffer> m_IndexBuffer;
+		std::shared_ptr<Shader> m_RGBShader;
+		std::shared_ptr<VertexArray> m_TriangleVA;
+
+		std::shared_ptr<Shader> m_BlueShader;
+		std::shared_ptr<VertexArray> m_SquareVA;
+
 	private:
 		static Application* m_Instance;
 	};
