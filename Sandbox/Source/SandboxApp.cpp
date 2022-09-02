3bf8e87235734447f6bc03b5efaae69bcfa1ad9e
Israel Santos Vieira <IsraelS_Vieira@hotmail.com>
2022-09-02 15:58:49 -0300

    Shaders transforms adicionados

--- Sandbox/Source/SandboxApp.cpp
+++ Sandbox/Source/SandboxApp.cpp
@@ -1,9 +1,11 @@
 #include <Vox.h>
 
+#include <glm/gtc/matrix_transform.hpp>
+
 class ExampleLayer : public Vox::Layer
 {
 public:
-	ExampleLayer() : Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f) ,
+	ExampleLayer() : Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f),
 		m_CameraPosition(0.0f)
 	{
 		m_TriangleVA.reset(Vox::VertexArray::Create());
@@ -32,10 +34,10 @@ public:
 		m_SquareVA.reset(Vox::VertexArray::Create());
 
 		float squareVertices[3 * 4] = {
-			-0.75f, -0.75, 0.0f,
-			0.75f, -0.75f, 0.0f,
-			0.75f, 0.75f, 0.0f,
-			-0.75f, 0.75f, 0.0f
+			-0.5f, -0.5, 0.0f,
+			0.5f, -0.5f, 0.0f,
+			0.5f, 0.5f, 0.0f,
+			-0.5f, 0.5f, 0.0f
 		};
 
 		std::shared_ptr<Vox::VertexBuffer> squareVB;
@@ -58,6 +60,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -66,7 +69,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -93,13 +96,14 @@ public:
 			layout(location = 0) in vec3 a_Position;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -156,7 +160,18 @@ public:
 
 		Vox::Renderer::BeginScene(m_Camera);
 
-		Vox::Renderer::Submit(m_BlueShader, m_SquareVA);
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for (int y = 0; y < 20; y++)
+		{
+			for (int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				Vox::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+			}
+		}
+		
 		Vox::Renderer::Submit(m_RGBShader, m_TriangleVA);
 
 		Vox::Renderer::EndScene();
@@ -178,8 +193,9 @@ private:
 
 	Vox::OrthographicCamera m_Camera;
 	glm::vec3 m_CameraPosition;
-	float m_CameraRotation = 0.0f;
 	float m_CameraMoveSpeed = 5.0f;
+
+	float m_CameraRotation = 0.0f;
 	float m_CameraRotationSpeed = 180.0f;
 };
 
