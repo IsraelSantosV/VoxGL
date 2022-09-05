0d52e970d588360157c9bf2bfbdfe012630f99a1
Israel Santos Vieira <IsraelS_Vieira@hotmail.com>
2022-09-05 11:19:44 -0300

    Shader Assets adicionados

--- VoxGL/Source/Vox/Renderer/Shader.cpp
+++ VoxGL/Source/Vox/Renderer/Shader.cpp
@@ -6,6 +6,21 @@
 
 namespace Vox
 {
+	Shader* Shader::Create(const std::string& filepath)
+	{
+		switch (Renderer::GetAPI())
+		{
+			case RendererAPI::API::None:
+				VOX_CORE_ASSERT(false, "RendererApi::None is currently not supported!");
+				return nullptr;
+			case RendererAPI::API::OpenGL:
+				return new OpenGLShader(filepath);
+		}
+
+		VOX_CORE_ASSERT(false, "Unknown RendererApi!");
+		return nullptr;
+	}
+
 	Shader* Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc)
 	{
 		switch (Renderer::GetAPI())
