424928bfe037366580dd5bbcf7084769f1ef4caa
Israel Santos Vieira <IsraelS_Vieira@hotmail.com>
2022-09-05 18:04:22 -0300

    Redimensionamento de Câmera adicionado

--- VoxGL/Source/Vox/Renderer/RenderCommand.h
+++ VoxGL/Source/Vox/Renderer/RenderCommand.h
@@ -12,6 +12,11 @@ namespace Vox
 			m_RendererAPI->Init();
 		}
 
+		inline static void SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height)
+		{
+			m_RendererAPI->SetViewport(x, y, width, height);
+		}
+
 		inline static void SetClearColor(const glm::vec4& color)
 		{
 			m_RendererAPI->SetClearColor(color);
